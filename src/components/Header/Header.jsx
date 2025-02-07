6006c3e635e55abbb60d59bfbefbb3815d5cd49c
Koda125 <dcspurlin@gmail.com>
2025-02-06 20:32:30 -0600

    Initial commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import useStore from "../../zustand/store";
+import { useState } from "react";
+import Nav from "../Nav/Nav";
+
+function Header( ){
+
+
+    return (
+        <>
+            <h1>Question Everything, Everyone...</h1>
+        </>
+    )
+}
+
+export default Header;
