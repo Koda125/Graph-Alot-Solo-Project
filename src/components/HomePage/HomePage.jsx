245ce02a8fd4af87166f5aef4aaccf7a594e7f3c
Dakodah Spurlin <dcspurlin@gmail.com>
2025-02-10 14:24:45 -0600

    Placeholder image, and early select options have been set up.

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,18 +1,47 @@
+
 import useStore from '../../zustand/store'
+import './HomePage.css'
+import { useState } from 'react';
 
 
 function HomePage() {
   const user = useStore((state) => state.user);
   const logOut = useStore((state) => state.logOut);
+  const [ defaulto, setDefaulto ] = useState(false)
+  const [ C , setC ] = useState(false)
+  
 
   return (
     <>
-      <h2>Home Page</h2>
-      <p>Your username is: {user.username}</p>
-      <p>Your ID is: {user.id}</p>
-      <button onClick={logOut}>
-        Log Out
-      </button>
+    <div>
+      <img src='https://placekeanu.com/400/400' className='placeholder'></img>
+      
+    </div>
+      <select onChange={(e)=>{ setDefaulto(true)}}>
+        <option > Please select one of the following funcitons</option>
+        <option > y = Ax + B</option>
+        <option > y = Ax^2 + B </option>
+        <option onSelect={(e)=>{ setC(true)}}> y = Ax^2 + Bx + C</option>
+      </select>
+      <div>
+      {defaulto ? (
+      <div>
+        <p>
+          Please give A a value: <input placeholder='Default = 1' type='number'></input>
+        </p>
+        <p>
+          Please give B a value: <input placeholder='Default = 0' type='number'></input>
+        </p>
+        { C ? (
+          <p>Testing 1..2..3..</p>
+        ) : (
+          <p></p>
+        )}
+      </div>
+      ) : (
+        <p>Please make a selection to proceed.</p>
+      )}
+      </div>
     </>
   );
 }
