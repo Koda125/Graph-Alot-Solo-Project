d10dd694e48a1445df5418b186e9fd2c8193a99c
Dakodah Spurlin <dcspurlin@gmail.com>
2025-02-20 14:29:17 -0600

    Implemented an if clause for starting point to be drawn to end point.

--- src/CanvasGraph/CanvasGraph.jsx
+++ src/CanvasGraph/CanvasGraph.jsx
@@ -17,26 +17,44 @@ const CanvasGraph = ( props ) => {
         // begin to draw lines of a cartisian graph.
         context.beginPath();
         context.strokeStyle = "black"
-        context.moveTo( 0, 504);
-        context.lineTo(0, -504);
-        context.moveTo(-504, 0);
-        context.lineTo(504, 0);
+        context.moveTo( 0, 252);
+        context.lineTo(0, -252);
+        context.moveTo(-252, 0);
+        context.lineTo(252, 0);
         context.stroke();
     }
 
     useEffect(() => {
         const canvas = ref.current;
-        if (canvas) {
+        const context = canvas.getContext('2d');
+        DrawGraph();
+        const handleMouseMove = (event) => {
+          const rect = canvas.getBoundingClientRect();
+          const x = (canvas.width / 2) - (event.clientX - rect.left);
+          const y = (canvas.width / 2) - (event.clientY - rect.top);
+          console.log(`x: ${x}, y: ${y}`);
     
-        DrawGraph()
-        }
+          // Optional: Draw a small circle at the mouse position
+          
+          context.beginPath();
+          context.arc(x, y, 5, 10, 2 * Math.PI);
+          context.fillStyle = 'red';
+          context.fill();
+        };
     
-    }, [])
+        canvas.addEventListener('mousemove', handleMouseMove);
+    
+        return () => {
+          canvas.removeEventListener('mousemove', handleMouseMove);
+        };
+      }, []);
     function DrawLine(valueA, valueB, valueC) {
         console.log(valueA, valueB, valueC);
         
         const canvas = ref.current;
         const context = canvas.getContext('2d');
+
+        DrawGraph();
         
         if (props.passedItems.option === '1') {
             
@@ -44,22 +62,33 @@ const CanvasGraph = ( props ) => {
             context.strokeStyle = 'red';
             
             
-            let startX = -504;
-            let startY = (valueA * startX) + valueB;
+            // let startX = 504;
+            // let startY = ( canvas.height / 2 ) - ((valueA * startX) + valueB);
             
             
-            context.moveTo(startX, startY);
+            // context.moveTo(startX, startY);
             
-            for (let i = -504; i <= 504; i++) {
-                const x = i;
-                const y = (valueA * x) + valueB;  
-                context.lineTo(x, y);  
-
+            for (let i = -252; i <= 252; i++) {
                 
+                const y = ((valueA * i) + valueB);  
+                console.log("What values are being used?", valueA, valueB )
+
+                //Translate the y-coordinate to the new center of the canvas:
+                const translatedY = ( canvas.height / 2 ) - y;
+                console.log('What is the value for translatedY: ', translatedY, "When i= ", i)
+
+                if( i === -252){
+                    //When i is equal to the lower bound of the for loop:
+                context.moveTo(i, y);  
+                } else {
+                    //When i is anything buy the lower bound.
+                    context.lineTo(i, y)
+                }
+                //Draw the line:
+                context.stroke();
             }
     
             
-            context.stroke();
         } else {
             console.log('Oops, nothing happened... The option that was selected was: ', props.passedItems.option);
         }
